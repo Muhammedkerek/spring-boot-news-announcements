b3b810c93c670564aa38d93182b4b5895751d674
Muhammed <muhammedkerek2000@gmail.com>
2024-08-07 12:33:06 +0300

    added the user controller , fixed some errors and did some basic styling

---
+++ src/main/java/news/example/news/Config/WebConfig.java
@@ -0,0 +1,15 @@
+package news.example.news.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:uploads/");
+    }
+}
--- src/main/java/news/example/news/Resources/UserController.java
+++ src/main/java/news/example/news/Resources/UserController.java
@@ -1,13 +1,42 @@
 package news.example.news.Resources;
 
 
+import news.example.news.DOMAIN.Announcement;
+import news.example.news.DOMAIN.News;
+import news.example.news.Services.AnnouncementService;
+import news.example.news.Services.NewsService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class UserController {
+    private final NewsService newsService;
+    private final AnnouncementService announcementService;
+
+    public UserController(NewsService newsService, AnnouncementService announcementService) {
+        this.newsService = newsService;
+        this.announcementService = announcementService;
+    }
+
     @GetMapping("/user")
     public String adminPage() {
         return "user";
     }
+    @GetMapping("/user/news")
+    public String listNews(ModelMap model) {
+        List<News> newsList = newsService.getAllNews();
+        System.out.println("News List: " + newsList);
+        model.put("newsList", newsList);
+        return "user-news";
+    }
+    @GetMapping("/Announcements")
+    public String listAnnouncements(ModelMap model) {
+        List<Announcement> announcementsList = announcementService.listAllAnnouncements();
+        model.put("announcementsList", announcementsList);
+        return "user-announcements";
+    }
+
 }
